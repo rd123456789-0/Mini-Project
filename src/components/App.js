3199175603e9a25ea0777fd8a68a470084827fd8
Rishi Daryani <rishidaryani968@gmail.com>
2024-02-04 00:49:41 +0530

    Added Quiz section

--- src/components/App.js
+++ src/components/App.js
@@ -15,8 +15,8 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/quiz" element={<Quiz />} />
-          <Route path="/result" element={<Result />} /> 
+          <Route path="/quiz/:id" element={<Quiz />} />
+          <Route path="/result/:id" element={<Result />} /> 
         </Routes>
       </Layout>
     </AuthProvider>
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -13,16 +13,18 @@ export default function Videos() {
         <InfiniteScroll
           dataLength={videos.length}
           hasMore={hasMore}
+          loader="Loading..."
           next={() => setPage(page + 8)}
         >
           {videos.map((video) =>
             video.noq > 0 ? (
-              <Link to="/quiz" key={video.youtubeID}>
+              <Link to={`/quiz/${video.youtubeID}`} key={video.youtubeID}>
                 <Video
-                  title={video.title}
-                  id={video.youtubeID}
-                  noq={video.noq}
-                />
+                title={video.title}
+                id={video.youtubeID}
+                noq={video.noq}
+                key={video.youtubeID}
+              />
               </Link>
             ) : (
               <Video title={video.title} id={video.youtubeID} noq={video.noq} />
