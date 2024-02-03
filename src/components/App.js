a100be79d7768a43a5ba69088c320cd9b5ced505
Rishi Daryani <rishidaryani968@gmail.com>
2024-02-04 00:38:11 +0530

    Added VideoList from database to home page

--- src/components/App.js
+++ src/components/App.js
@@ -11,18 +11,14 @@ export default function App() {
   return (
     <AuthProvider >
       <Layout>
-    <Routes>
-      
-        
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/quiz" element={<Quiz />} />
-          <Route path="/result" element={<Result />} />
-        
-      
-    </Routes>
-    </Layout>
+          <Route path="/result" element={<Result />} /> 
+        </Routes>
+      </Layout>
     </AuthProvider>
   );
 }
