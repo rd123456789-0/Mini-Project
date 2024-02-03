c5e7901122e8b6417765268a152c3eac0a4eb6e7
Rishi Daryani <rishidaryani968@gmail.com>
2024-02-03 20:19:03 +0530

    Added routes for all the pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
 import "../styles/App.css";
 import Layout from "./Layout";
 import Result from "./pages/Result";
-// import Home from "./pages/Home";
-// import Signup from "./pages/Signup";
-// import Login from "./pages/Login";
-// import Quiz from "./pages/Quiz";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
+import { Route, Routes } from "react-router-dom";
 export default function App() {
   return (
     <Layout>
-      {/* <Home /> */}
-      {/* <Signup /> */}
-      {/* <Login /> */}
-      {/* <Quiz /> */}
-      <Result />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
     </Layout>
   );
 }
