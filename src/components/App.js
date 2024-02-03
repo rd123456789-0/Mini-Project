c592ca7f194568d6161924ee90053c51c70a5841
Rishi Daryani <rishidaryani968@gmail.com>
2024-02-03 21:01:10 +0530

    Firebase Setup Complete

--- src/components/App.js
+++ src/components/App.js
@@ -6,16 +6,23 @@ import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
 import { Route, Routes } from "react-router-dom";
+import { AuthProvider } from "../contexts/AuthContext";
 export default function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/result" element={<Result />} />
-      </Routes>
+    <AuthProvider >
+      <Layout>
+    <Routes>
+      
+        
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
+        
+      
+    </Routes>
     </Layout>
+    </AuthProvider>
   );
 }
