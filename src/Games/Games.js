fbaa35aeff41b0ac81f91f6eb92cb79f7b3486ba
Rishi Daryani <rishidaryani968@gmail.com>
2024-04-11 15:24:21 +0530

    Added Tic Tac Toe Game

--- src/Games/Games.js
+++ src/Games/Games.js
@@ -1,8 +1,24 @@
-import React from 'react'
-import { MathGame } from './MathGame/MathGame'
+import React from "react";
+import { useState } from "react";
+import { MathGame } from "./MathGame/MathGame";
+import TicTacToe from "./TicTacToe/TicTacToe";
 
 export const Games = () => {
+  const [game, setGame] = useState(null);
   return (
-    <MathGame />
-  )
-}
+    <>
+      {game === null && (
+        <div>
+          <button onClick={() => setGame("MathGame")}>MathsGame</button>
+        </div>
+      )}
+      {game === null && (
+        <div>
+          <button onClick={() => setGame("TicTacToe")}>TicTacToe</button>
+        </div>
+      )}
+      {game === "MathGame" && <MathGame />}
+      {game === "TicTacToe" && <TicTacToe />}
+    </>
+  );
+};
